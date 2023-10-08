3eac0e7d17e469bbf461b504a423dcec8d01eac9
ANCHIT RANA <70859887+PHANTOM9009@users.noreply.github.com>
2023-10-08 14:24:47 +0530

    Revert "test failed, we cannot use try lock in this"
    This reverts commit b43b5c8c87b11e14c04cf9dd9eb217e9c294bea1.

--- server_v3.cpp
+++ server_v3.cpp
@@ -636,7 +636,7 @@ void recv_data_terminal(Mutex* m)
 					std::memcpy(&ob, all_buffer[id], sizeof(ob));
 					if (ob.st == 100 && ob.end == 101)
 					{
-						unique_lock<mutex> lk(m->recv_ship[id]);
+						unique_lock<mutex> lk(m->recv_terminal);
 						input_data[id].push_back(ob);
 						cout << "\n came here";
 						count++;
@@ -686,7 +686,7 @@ void recv_data_terminal(Mutex* m)
 					{
 						
 						count++;
-						unique_lock<mutex> lk(m->recv_ship[id]);
+						unique_lock<mutex> lk(m->recv_terminal);
 						input_data[id].push_back(ob);
 					}
 					else
@@ -1799,52 +1799,50 @@ void graphics::callable(Mutex* mutx, int code[rows][columns], Map& map_ob, int n
 					
 					continue;
 				}
+				
+				unique_lock<mutex> lk(mutx->recv_terminal);
 				int gone = 0;
-				if (mutx->recv_ship[j].try_lock())
+				
+				if (input_data[j].size()>0)
 				{
 
-					if (input_data[j].size() > 0)
+					input_size += input_data[j].size();
+					total_times++;
+
+					send_data data2;
+					memset((void*)&data2, 0, sizeof(data2));
+					data2 = input_data[j][0];
+					input_data[j].pop_front();
+					lk.unlock();
+					int sid = data2.shipdata_forServer.ship_id;
+					gone = 1;
+					//cout << "\n received data from the client=>" << data2.shipdata_forServer.ship_id;
+					for (int k = 0; k < data2.shipdata_forServer.size_upgrade_data; k++)
 					{
-
-						input_size += input_data[j].size();
-						total_times++;
-
-						send_data data2;
-						memset((void*)&data2, 0, sizeof(data2));
-						data2 = input_data[j][0];
-						input_data[j].pop_front();
-						mutx->recv_ship[j].unlock();
-						int sid = data2.shipdata_forServer.ship_id;
-						gone = 1;
-						//cout << "\n received data from the client=>" << data2.shipdata_forServer.ship_id;
-						for (int k = 0; k < data2.shipdata_forServer.size_upgrade_data; k++)
+						if (data2.shipdata_forServer.udata[k].type == 1)
 						{
-							if (data2.shipdata_forServer.udata[k].type == 1)
-							{
-								cout << "\n health upgrade came from =>" << sid << "  packet id=>" << data2.packet_id;
-							}
-
+							cout << "\n health upgrade came from =>" << sid << "  packet id=>" << data2.packet_id;
 						}
-						control.server_to_myData(data2.shipdata_forServer, pl1, sid, mutx);
-						prev_packet_id[sid] = data2.packet_id;
-						if (sid == 1)//for 1 only
-						{
-							auto now = std::chrono::system_clock::now();
-							auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
-							auto secs = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()) % 60;
-							auto mins = std::chrono::duration_cast<std::chrono::minutes>(now.time_since_epoch()) % 60;
-							auto hours = std::chrono::duration_cast<std::chrono::hours>(now.time_since_epoch());
 
-							//cout << "\n recved data from client terminal=>" << data2.packet_id << " at the time==> " <<
-							//hours.count() << ":" << mins.count() << ":" << secs.count() << ":" << ms.count() << endl;
-						}
 					}
-					if (gone == 0)
+					control.server_to_myData(data2.shipdata_forServer, pl1, sid, mutx);
+					prev_packet_id[sid] = data2.packet_id;
+					if (sid == 1)//for 1 only
 					{
-						mutx->recv_ship[j].unlock();
+						auto now = std::chrono::system_clock::now();
+						auto ms = std::chrono::duration_cast<std::chrono::milliseconds>(now.time_since_epoch()) % 1000;
+						auto secs = std::chrono::duration_cast<std::chrono::seconds>(now.time_since_epoch()) % 60;
+						auto mins = std::chrono::duration_cast<std::chrono::minutes>(now.time_since_epoch()) % 60;
+						auto hours = std::chrono::duration_cast<std::chrono::hours>(now.time_since_epoch());
+
+						//cout << "\n recved data from client terminal=>" << data2.packet_id << " at the time==> " <<
+						//hours.count() << ":" << mins.count() << ":" << secs.count() << ":" << ms.count() << endl;
 					}
 				}
-				
+				if (gone == 0)
+				{
+					lk.unlock();
+				}
 
 			}
 			
